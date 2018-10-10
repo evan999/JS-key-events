3b2495f8744a09222fc86f3384fc356498293c5d
RNMancini <rebeccamancini.student@careerdevs.com>
2018-10-10 03:11:22 +0000

    Event listeners and response alert test

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  alert('You pressed the ' + keyName + ' key');
+  
+  var msg = document.getElementById('msg');
+  if(event.which == 13){
+    msg.innerHTML = 'The enter key was pressed';
+  }
+  else if(event.which == 38){
+    msg.style.display = 'none';
+  }
+  else if(event.which == 65){
+    document.body.style.backgroundColor = 'blue';
+  }
+  else if(event.which == 88){
+    window.close();  
+  }
+    
+});
